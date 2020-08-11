7353882cceaea17a8393e8607cf5af49c2c64ac9
plvhx <gandung@slackerc0de.us>
2020-08-12 03:44:03 +0700

    Adding src/Service/Order.php

---
+++ src/Service/Order.php
@@ -0,0 +1,226 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gandung\Tokopedia\Service;
+
+use InvalidArgumentException;
+use Gandung\Tokopedia\AbstractService;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+class Order extends AbstractService
+{
+	/**
+	 * @var int
+	 */
+	const ORDER_SELLER_ACCEPT_ORDER = 400;
+
+	/**
+	 * @var int
+	 */
+	const ORDER_SELLER_REJECT_ORDER = 10;
+
+	/**
+	 * @var int
+	 */
+	const ORDER_SHIPMENT = 500;
+
+	/**
+	 * @var int
+	 */
+	const ORDER_REJECT_SHIPPING_CASE = 0;
+
+	/**
+	 * @var int
+	 */
+	const ORDER_REJECT_PRODUCT_OUT_OF_STOCK = 1;
+
+	/**
+	 * @var int
+	 */
+	const ORDER_REJECT_PRODUCT_VARIANT_UNAVAILABLE = 2;
+
+	/**
+	 * @var int
+	 */
+	const ORDER_REJECT_WRONG_PRICE_OR_WEIGHT = 3;
+
+	/**
+	 * @var int
+	 */
+	const ORDER_REJECT_SHOP_CLOSED = 4;
+
+	/**
+	 * @var int
+	 */
+	const ORDER_REJECT_OTHERS = 5;
+
+	/**
+	 * @var int
+	 */
+	const ORDER_REJECT_COURIER_PROBLEM = 7;
+
+	/**
+	 * @var int
+	 */
+	const ORDER_REJECT_BUYER_REQUEST = 8;
+
+	public function getAllOrders(
+		int $fromDate,
+		int $toDate,
+		int $page,
+		int $perPage,
+		int $shopID = 0,
+		int $warehouseID = 0,
+		int $status = 0
+	) {
+		$this->setEndpoint('/v2/order/list');
+
+		if ($page < 1) {
+			// throw something..
+		}
+
+		if ($shopID > 0 && $warehouseID > 0) {
+			// throw something..
+		}
+
+		if ($status !== 0) {
+			$this->validateOrderStatusCode($status);
+		}
+
+		$queryParams              = [];
+		$queryParams['fs_id']     = $this->getFulfillmentServiceID();
+		$queryParams['from_date'] = $fromDate;
+		$queryParams['to_date']   = $toDate;
+		$queryParams['page']      = $page;
+		$queryParams['per_page']  = $perPage;
+
+		if ($shopID !== 0) {
+			$queryParams['shop_id'] = $shopID;	
+		}
+
+		if ($warehouseID !== 0) {
+			$queryParams['warehouse_id'] = $warehouseID;
+		}
+
+		$queryParams['status'] = $status;
+
+		return $this->getHttpClient()->request(
+			'GET',
+			sprintf(
+				'%s?%s',
+				$this->getEndpoint(),
+				http_build_query($queryParams)
+			)
+		);
+	}
+
+	public function getSingleOrder(int $orderID, string $invoiceNo = '')
+	{
+		$this->setEndpoint(
+			sprintf(
+				'/v2/fs/%s/order',
+				$this->getFulfillmentServiceID()
+			)
+		);
+
+		$queryParams             = [];
+		$queryParams['order_id'] = $orderID;
+
+		if ($invoiceNo !== '') {
+			$queryParams['invoice_num'] = $invoiceNo;
+		}
+
+		return $this->getHttpClient()->request(
+			'GET',
+			sprintf(
+				'%s?%s',
+				$this->getEndpoint(),
+				http_build_query($queryParams)
+			)
+		);
+	}
+
+	public function getShippingLabel(int $orderID, int $printed = 0)
+	{
+		$this->setEndpoint(
+			sprintf(
+				'/v1/order/%d/fs/%s/shipping-label',
+				$orderID,
+				$this->getFulfillmentServiceID()
+			)
+		);
+
+		$queryParams            = [];
+		$queryParams['printed'] = $printed;
+
+		return $this->getHttpClient()->request(
+			'GET',
+			sprintf(
+				'%s?%s',
+				$this->getEndpoint(),
+				http_build_query($queryParams)
+			)
+		);
+	}
+
+	public function acceptOrder(int $orderID)
+	{
+		$this->setEndpoint(
+			sprintf(
+				'/v1/order/%d/fs/%s/ack',
+				$orderID,
+				$this->getFulfillmentServiceID()
+			)
+		);
+
+		return $this->getHttpClient()->request('POST', $this->getEndpoint());
+	}
+
+	public function rejectOrder(int $orderID, array $data)
+	{
+		$this->setEndpoint(
+			sprintf(
+				'/v1/order/%d/fs/%s/nack',
+				$orderID,
+				$this->getFulfillmentServiceID()
+			)
+		);
+	}
+
+	private function getAggregatedOrderStatusCodes()
+	{
+		return [
+			self::ORDER_SELLER_ACCEPT_ORDER,
+			self::ORDER_SELLER_REJECT_ORDER,
+			self::ORDER_SHIPMENT
+		];
+	}
+
+	private function getAggregatedOrderRejectStatusCodes()
+	{
+		return [
+			self::ORDER_REJECT_SHIPPING_CASE,
+			self::ORDER_REJECT_PRODUCT_OUT_OF_STOCK,
+			self::ORDER_REJECT_PRODUCT_VARIANT_UNAVAILABLE,
+			self::ORDER_REJECT_WRONG_PRICE_OR_WEIGHT,
+			self::ORDER_REJECT_SHOP_CLOSED,
+			self::ORDER_REJECT_OTHERS,
+			self::ORDER_REJECT_COURIER_PROBLEM,
+			self::ORDER_REJECT_BUYER_REQUEST
+		];
+	}
+
+	private function validateOrderStatusCode(int $status)
+	{
+		if (!in_array($status, $this->getAggregatedOrderStatusCodes(), true)) {
+			throw new InvalidArgumentException(
+				"Invalid order status code."
+			);
+		}
+
+		return;
+	}
+}
