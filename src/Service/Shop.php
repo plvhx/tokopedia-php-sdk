d2397d055fb2a76870858d0833500f28a9e66692
plvhx <gandung@slackerc0de.us>
2020-08-12 23:09:52 +0700

    Refactoring src/Service/Shop.php

--- src/Service/Shop.php
+++ src/Service/Shop.php
@@ -11,6 +11,12 @@ use Gandung\Tokopedia\AbstractService;
  */
 class Shop extends AbstractService
 {
+	/**
+	 * Get shop info.
+	 *
+	 * @param int $shopID Shop ID.
+	 * @return string
+	 */
 	public function getShopInfo(int $shopID = 0)
 	{
 		$this->setEndpoint(
@@ -20,16 +26,37 @@ class Shop extends AbstractService
 			)
 		);
 
-		$queryParams            = [];
-		$queryParams['shop_id'] = $shopID;
+		$queryParams = [];
+
+		if ($shopID > 0) {
+			$queryParams['shop_id'] = $shopID;
+		}
 
 		return $this->getHttpClient()->request(
 			'GET',
 			sprintf(
-				'%s?%s',
+				'%s%s',
 				$this->getEndpoint(),
-				http_build_query($queryParams)
+				sizeof($queryParams) === 0 ? '' : '?' . http_build_query($queryParams)
 			)
 		);
+	}
+
+	/**
+	 * Update shop status.
+	 *
+	 * @param array $data
+	 * @return string
+	 */
+	public function updateShopStatus(array $data)
+	{
+		$this->setEndpoint(
+			sprintf(
+				'/v2/shop/fs/%s/shop-status',
+				$this->getFulfillmentServiceID()				
+			)
+		);
+
+		return $this->getHttpClient()->request('POST', $this->getEndpoint(), $data);
 	}
 }
