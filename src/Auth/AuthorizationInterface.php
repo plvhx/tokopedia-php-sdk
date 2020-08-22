d9fd4b481fb1990ece66b1bc6183212b74ad924a
plvhx <gandung@slackerc0de.us>
2020-08-23 06:56:24 +0700

    Adding src/Auth/AuthorizationInterface.php

---
+++ src/Auth/AuthorizationInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gandung\Tokopedia\Auth;
+
+use Gandung\Tokopedia\ServiceInterface;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+interface AuthorizationInterface extends ServiceInterface
+{
+}
