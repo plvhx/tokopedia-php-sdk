bee57a584bc3800bef3719fe02d26db9c56583a5
plvhx <gandung@slackerc0de.us>
2020-08-23 07:03:05 +0700

    Refactoring src/Auth/Authorization.php

--- src/Auth/Authorization.php
+++ src/Auth/Authorization.php
@@ -6,6 +6,9 @@ namespace Gandung\Tokopedia\Auth;
 
 use Gandung\Tokopedia\AbstractService;
 
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
 class Authorization extends AbstractService implements AuthorizationInterface
 {
 }
