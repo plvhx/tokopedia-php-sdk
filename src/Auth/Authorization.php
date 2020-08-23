d31686ed27c7113c895ffa0de3697270e5036e48
plvhx <gandung@slackerc0de.us>
2020-08-23 07:00:23 +0700

    Adding src/Auth/Authorization.php

---
+++ src/Auth/Authorization.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gandung\Tokopedia\Auth;
+
+use Gandung\Tokopedia\AbstractService;
+
+class Authorization extends AbstractService implements AuthorizationInterface
+{
+}
