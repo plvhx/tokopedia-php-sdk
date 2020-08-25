cb4e29b466df12c52a5cc51ef33f176c1033606f
plvhx <gandung@slackerc0de.us>
2020-08-25 10:47:20 +0700

    Refactoring src/Service/Logistic.php

--- src/Service/Logistic.php
+++ src/Service/Logistic.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Gandung\Tokopedia\Service;
 
-use Gandung\Tokopedia\AbstractService;
-
 /**
  * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
  */
-class Logistic extends AbstractService
+class Logistic extends Resource
 {
 	/**
 	 * Get shipment info.
@@ -19,17 +17,22 @@ class Logistic extends AbstractService
 	 */
 	public function getShipmentInfo(int $shopID)
 	{
-		$endpoint = sprintf(
+		$credential = $this->getAuthorization()->authorize();
+		$endpoint   = sprintf(
 			'/v2/logistic/fs/%s/info',
 			$this->getFulfillmentServiceID()
 		);
+		$headers    = [
+			'Authorization' => sprintf("Bearer %s", $credential->getAccessToken())
+		];
 
 		$queryParams            = [];
 		$queryParams['shop_id'] = $shopID;
 
 		return $this->getHttpClient()->request(
 			'GET',
-			sprintf('%s?%s', $endpoint, http_build_query($queryParams))
+			sprintf('%s?%s', $endpoint, http_build_query($queryParams)),
+			['headers' => $headers]
 		);
 	}
 }
