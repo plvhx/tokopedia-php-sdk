ce7178b071fe6674a488f967dadfdcd4a70db3e3
plvhx <gandung@slackerc0de.us>
2020-08-28 22:11:35 +0700

    Adding src/ServiceFactory.php

---
+++ src/ServiceFactory.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gandung\Tokopedia;
+
+use Gandung\Tokopedia\Auth\AuthorizationInterface;
+use Gandung\Tokopedia\Service\Campaign;
+use Gandung\Tokopedia\Service\Category;
+use Gandung\Tokopedia\Service\Interaction;
+use Gandung\Tokopedia\Service\Logistic;
+use Gandung\Tokopedia\Service\Order;
+use Gandung\Tokopedia\Service\Product;
+use Gandung\Tokopedia\Service\Shop;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+final class ServiceFactory implements ServiceFactoryInterface
+{
+	use ServiceFactoryTrait;
+
+	/**
+	 * @var AuthorizationInterface
+	 */
+	private $authorization;
+
+	public function __construct(AuthorizationInterface $authorization)
+	{
+		$this->setAuthorization($authorization);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getCampaign()
+	{
+		return new Campaign($this->getAuthorization());
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getCategory()
+	{
+		return new Category($this->getAuthorization());
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getInteraction()
+	{
+		return new Interaction($this->getAuthorization());
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getLogistic()
+	{
+		return new Logistic($this->getAuthorization());
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getOrder()
+	{
+		return new Order($this->getAuthorization());
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getProduct()
+	{
+		return new Product($this->getAuthorization());
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getShop()
+	{
+		return new Shop($this->getAuthorization());
+	}
+}
