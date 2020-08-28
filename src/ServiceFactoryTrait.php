49497c2b2d21a3d5ca1ad82f5daa9c54dc9e0bca
plvhx <gandung@slackerc0de.us>
2020-08-28 22:18:54 +0700

    Adding src/ServiceFactoryTrait.php

---
+++ src/ServiceFactoryTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gandung\Tokopedia;
+
+use Gandung\Tokopedia\Auth\AuthorizationInterface;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+trait ServiceFactoryTrait
+{
+	/**
+	 * Get authorization object.
+	 *
+	 * @return AuthorizationInterface
+	 */
+	private function getAuthorization()
+	{
+		return $this->authorization;
+	}
+
+	/**
+	 * Set authorization object.
+	 *
+	 * @param AuthorizationInterface $authorization
+	 * @return void
+	 */
+	private function setAuthorization(AuthorizationInterface $authorization)
+	{
+		$this->authorization = $authorization;
+	}
+}
