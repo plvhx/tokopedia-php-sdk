e426a136d159362edb6499ed24763b16bca1526e
plvhx <gandung@slackerc0de.us>
2020-08-28 15:10:25 +0700

    Refactoring src/Service/Resource.php

--- src/Service/Resource.php
+++ src/Service/Resource.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Gandung\Tokopedia\Service;
 
+use GuzzleHttp\Client;
 use Gandung\Tokopedia\AbstractService;
 use Gandung\Tokopedia\Auth\AuthorizationInterface;
 
@@ -22,10 +23,29 @@ abstract class Resource extends AbstractService
 	 * @param array $config
 	 * @return void
 	 */
-	public function __construct(AuthorizationInterface $authorization, array $config = [])
+	public function __construct(AuthorizationInterface $authorization)
 	{
-		parent::__construct($config);
+		$this->initialize($authorization);
+	}
+
+	/**
+	 * Initialize current class constructor.
+	 *
+	 * @param AuthorizationInterface $authorization
+	 * @return void
+	 */
+	private function initialize(AuthorizationInterface $authorization)
+	{
 		$this->setAuthorization($authorization);
+		$this->setBaseUrl($this->getDefaultBaseUrl());
+		$this->setFulfillmentServiceID($authorization->getFulfillmentServiceID());
+		$this->setClientID($authorization->getClientID());
+		$this->setClientSecret($authorization->getClientSecret());
+		$this->setHttpClient(
+			new Client([
+				'base_uri' => $this->getBaseUrl()
+			])
+		);
 	}
 
 	/**
@@ -47,5 +67,34 @@ abstract class Resource extends AbstractService
 	public function setAuthorization(AuthorizationInterface $authorization)
 	{
 		$this->authorization = $authorization;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function getDefaultBaseUrl()
+	{
+		return 'https://fs.tokopedia.net';
+	}
+
+	/**
+	 * Process an authorized request.
+	 *
+	 * @param string $method
+	 * @param string $uri
+	 * @param array $data
+	 * @return ResponseInterface
+	 */
+	protected function call(string $method, string $uri, array $data = [])
+	{
+		$credential = $this->getAuthorization()->authorize();
+		$headers    = ['Authorization' => sprintf('Bearer %s', $credential->getAccessToken())];
+		$option     = ['headers' => $headers];
+
+		if (!empty($data)) {
+			$option['json'] = $data;
+		}
+
+		return $this->getHttpClient()->request($method, $uri, $option);
 	}
 }
