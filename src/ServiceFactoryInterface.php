a50a6783ac03631cd1e13e45ec515f807229af57
plvhx <gandung@slackerc0de.us>
2020-08-28 21:51:18 +0700

    Refactoring src/ServiceFactoryInterface.php

---
+++ src/ServiceFactoryInterface.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gandung\Tokopedia;
+
+/**
+ * @author Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
+ */
+interface ServiceFactoryInterface
+{
+	/**
+	 * Get associated service based on given type.
+	 *
+	 * @param int $type
+	 * @return ServiceInterface
+	 */
+	public function get(int $type);
+
+	/**
+	 * Get campaign service.
+	 *
+	 * @return ServiceInterface
+	 */
+	public function getCampaign();
+
+	/**
+	 * Get category service.
+	 *
+	 * @return ServiceInterface
+	 */
+	public function getCategory();
+
+	/**
+	 * Get interaction service.
+	 *
+	 * @return ServiceInterface
+	 */
+	public function getInteraction();
+
+	/**
+	 * Get logistic service.
+	 *
+	 * @return ServiceInterface
+	 */
+	public function getLogistic();
+
+	/**
+	 * Get order service.
+	 *
+	 * @return ServiceInterface
+	 */
+	public function getOrder();
+
+	/**
+	 * Get product service.
+	 *
+	 * @return ServiceInterface
+	 */
+	public function getProduct();
+
+	/**
+	 * Get shop service.
+	 *
+	 * @return ServiceInterface
+	 */
+	public function getShop();
+}
