6ea2369f71a79d3fede857a9dfceda997ad60165
Paulus Gandung Prakosa <rvn.plvhx@gmail.com>
2021-02-19 18:47:21 +0700

    Fixing wrapped 'webhooks' functionality code indentation based on PSR-2 conformance.

--- src/Service/Webhooks.php
+++ src/Service/Webhooks.php
@@ -9,53 +9,53 @@ namespace Gandung\Tokopedia\Service;
  */
 class Webhooks extends Resource
 {
-	/**
-	 * List all registered webhooks.
-	 *
-	 * @return string
-	 */
-	public function listRegisteredWebhooks()
-	{
-		$endpoint = sprintf(
-			'/v1/fs/%d',
-			$this->getFulfillmentServiceID()
-		);
-		$response = $this->call('GET', $endpoint);
+    /**
+     * List all registered webhooks.
+     *
+     * @return string
+     */
+    public function listRegisteredWebhooks()
+    {
+        $endpoint = sprintf(
+            '/v1/fs/%d',
+            $this->getFulfillmentServiceID()
+        );
+        $response = $this->call('GET', $endpoint);
 
-		return $this->getContents($response);
-	}
+        return $this->getContents($response);
+    }
 
-	/**
-	 * Register list of url webhooks.
-	 *
-	 * @param array $data
-	 * @return string
-	 */
-	public function registerWebhooks(array $data)
-	{
-		$endpoint = sprintf(
-			'/v1/fs/%d/register',
-			$this->getFulfillmentServiceID()
-		);
-		$response = $this->call('POST', $endpoint, $data);
+    /**
+     * Register list of url webhooks.
+     *
+     * @param array $data
+     * @return string
+     */
+    public function registerWebhooks(array $data)
+    {
+        $endpoint = sprintf(
+            '/v1/fs/%d/register',
+            $this->getFulfillmentServiceID()
+        );
+        $response = $this->call('POST', $endpoint, $data);
 
-		return $this->getContents($response);
-	}
+        return $this->getContents($response);
+    }
 
-	/**
-	 * Manually trigger webhook.
-	 *
-	 * @param array $data
-	 * @return string
-	 */
-	public function triggerWebhook(array $data)
-	{
-		$endpoint = sprintf(
-			'/v1/fs/%d/trigger',
-			$this->getFulfillmentServiceID()
-		);
-		$response = $this->call('POST', $endpoint, $data);
+    /**
+     * Manually trigger webhook.
+     *
+     * @param array $data
+     * @return string
+     */
+    public function triggerWebhook(array $data)
+    {
+        $endpoint = sprintf(
+            '/v1/fs/%d/trigger',
+            $this->getFulfillmentServiceID()
+        );
+        $response = $this->call('POST', $endpoint, $data);
 
-		return $this->getContents($response);
-	}
+        return $this->getContents($response);
+    }
 }
--- tests/Service/WebhooksTest.php
+++ tests/Service/WebhooksTest.php
@@ -18,156 +18,156 @@ use function json_decode;
  */
 class WebhooksTest extends TestCase
 {
-	use ServiceTestTrait;
+    use ServiceTestTrait;
 
-	public function testCanGetRegisteredWebhooksWithSuccessResponse()
-	{
-		$webhooks    = new Webhooks($this->getAuthorization());
-		$mockHandler = new MockHandler();
-		$httpClient  = new Client(['handler' => $mockHandler]);
-		$contents    = file_get_contents(
-			__DIR__ . '/../data-fixtures/webhooks/list-registered-webhooks-ok.json'
-		);
+    public function testCanGetRegisteredWebhooksWithSuccessResponse()
+    {
+        $webhooks    = new Webhooks($this->getAuthorization());
+        $mockHandler = new MockHandler();
+        $httpClient  = new Client(['handler' => $mockHandler]);
+        $contents    = file_get_contents(
+            __DIR__ . '/../data-fixtures/webhooks/list-registered-webhooks-ok.json'
+        );
 
-		$mockHandler->append(new Response(
-			200,
-			['Content-Type' => 'application/json'],
-			$contents
-		));
+        $mockHandler->append(new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $contents
+        ));
 
-		$webhooks->setHttpClient($httpClient);
+        $webhooks->setHttpClient($httpClient);
 
-		$deserialized = json_decode($contents, true);
-		$response     = json_decode($webhooks->listRegisteredWebhooks(), true);
+        $deserialized = json_decode($contents, true);
+        $response     = json_decode($webhooks->listRegisteredWebhooks(), true);
 
-		$this->assertEquals($deserialized, $response);
-	}
+        $this->assertEquals($deserialized, $response);
+    }
 
-	public function testCanRegisterWebhooksWithSuccessResponse()
-	{
-		$webhooks    = new Webhooks($this->getAuthorization());
-		$mockHandler = new MockHandler();
-		$httpClient  = new Client(['handler' => $mockHandler]);
-		$contents    = file_get_contents(
-			__DIR__ . '/../data-fixtures/webhooks/register-webhooks-ok.json'
-		);
+    public function testCanRegisterWebhooksWithSuccessResponse()
+    {
+        $webhooks    = new Webhooks($this->getAuthorization());
+        $mockHandler = new MockHandler();
+        $httpClient  = new Client(['handler' => $mockHandler]);
+        $contents    = file_get_contents(
+            __DIR__ . '/../data-fixtures/webhooks/register-webhooks-ok.json'
+        );
 
-		$mockHandler->append(new Response(
-			200,
-			['Content-Type' => 'application/json'],
-			$contents
-		));
+        $mockHandler->append(new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $contents
+        ));
 
-		$webhooks->setHttpClient($httpClient);
+        $webhooks->setHttpClient($httpClient);
 
-		$response = $webhooks->registerWebhooks([
-			'fs_id' => 31337,
-			'order_notification_url' => 'http://example.com/api/v1/order/notification',
-			'order_cancellation_url' => 'http://example.com/api/v1/order/cancellation',
-			'order_status_url' => 'http://example.com/api/v1/order/status',
-			'order_request_cancellation_url' => 'http://example.com/api/v1/order/req/cancel',
-			'chat_notification_url' => 'http://example.com/api/v1/chat/notification',
-			'product_creation_url' => 'http://example.com/api/v1/product/create',
-			'webhook_secret' => uniqid()
-		]);
+        $response = $webhooks->registerWebhooks([
+            'fs_id' => 31337,
+            'order_notification_url' => 'http://example.com/api/v1/order/notification',
+            'order_cancellation_url' => 'http://example.com/api/v1/order/cancellation',
+            'order_status_url' => 'http://example.com/api/v1/order/status',
+            'order_request_cancellation_url' => 'http://example.com/api/v1/order/req/cancel',
+            'chat_notification_url' => 'http://example.com/api/v1/chat/notification',
+            'product_creation_url' => 'http://example.com/api/v1/product/create',
+            'webhook_secret' => uniqid()
+        ]);
 
-		$deserialized = json_decode($contents, true);
-		$response     = json_decode($response, true);
+        $deserialized = json_decode($contents, true);
+        $response     = json_decode($response, true);
 
-		$this->assertEquals($deserialized, $response);
-	}
+        $this->assertEquals($deserialized, $response);
+    }
 
-	public function testCanRegisterWebhooksWithFailedResponse()
-	{
-		$webhooks    = new Webhooks($this->getAuthorization());
-		$mockHandler = new MockHandler();
-		$httpClient  = new Client(['handler' => $mockHandler]);
-		$contents    = file_get_contents(
-			__DIR__ . '/../data-fixtures/webhooks/register-webhooks-fail.json'
-		);
+    public function testCanRegisterWebhooksWithFailedResponse()
+    {
+        $webhooks    = new Webhooks($this->getAuthorization());
+        $mockHandler = new MockHandler();
+        $httpClient  = new Client(['handler' => $mockHandler]);
+        $contents    = file_get_contents(
+            __DIR__ . '/../data-fixtures/webhooks/register-webhooks-fail.json'
+        );
 
-		$mockHandler->append(new Response(
-			200,
-			['Content-Type' => 'application/json'],
-			$contents
-		));
+        $mockHandler->append(new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $contents
+        ));
 
-		$webhooks->setHttpClient($httpClient);
+        $webhooks->setHttpClient($httpClient);
 
-		$response = $webhooks->registerWebhooks([
-			'fs_id' => 31337,
-			'order_notification_url' => 'http://example.com/api/v1/order/notification',
-			'order_cancellation_url' => 'http://example.com/api/v1/order/cancellation',
-			'order_status_url' => 'http://example.com/api/v1/order/status',
-			'order_request_cancellation_url' => 'http://example.com/api/v1/order/req/cancel',
-			'chat_notification_url' => 'http://example.com/api/v1/chat/notification',
-			'product_creation_url' => 'http://example.com/api/v1/product/create',
-			'webhook_secret' => uniqid()
-		]);
+        $response = $webhooks->registerWebhooks([
+            'fs_id' => 31337,
+            'order_notification_url' => 'http://example.com/api/v1/order/notification',
+            'order_cancellation_url' => 'http://example.com/api/v1/order/cancellation',
+            'order_status_url' => 'http://example.com/api/v1/order/status',
+            'order_request_cancellation_url' => 'http://example.com/api/v1/order/req/cancel',
+            'chat_notification_url' => 'http://example.com/api/v1/chat/notification',
+            'product_creation_url' => 'http://example.com/api/v1/product/create',
+            'webhook_secret' => uniqid()
+        ]);
 
-		$deserialized = json_decode($contents, true);
-		$response     = json_decode($response, true);
+        $deserialized = json_decode($contents, true);
+        $response     = json_decode($response, true);
 
-		$this->assertEquals($deserialized, $response);
-	}
+        $this->assertEquals($deserialized, $response);
+    }
 
-	public function testCanTriggerWebhookManuallyWithSuccessResponse()
-	{
-		$webhooks    = new Webhooks($this->getAuthorization());
-		$mockHandler = new MockHandler();
-		$httpClient  = new Client(['handler' => $mockHandler]);
-		$contents    = file_get_contents(
-			__DIR__ . '/../data-fixtures/webhooks/trigger-webhook-ok.json'
-		);
+    public function testCanTriggerWebhookManuallyWithSuccessResponse()
+    {
+        $webhooks    = new Webhooks($this->getAuthorization());
+        $mockHandler = new MockHandler();
+        $httpClient  = new Client(['handler' => $mockHandler]);
+        $contents    = file_get_contents(
+            __DIR__ . '/../data-fixtures/webhooks/trigger-webhook-ok.json'
+        );
 
-		$mockHandler->append(new Response(
-			200,
-			['Content-Type' => 'application/json'],
-			$contents
-		));
+        $mockHandler->append(new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $contents
+        ));
 
-		$webhooks->setHttpClient($httpClient);
+        $webhooks->setHttpClient($httpClient);
 
-		$response = $webhooks->triggerWebhook([
-			'type' => 'order_notification',
-			'order_id' => 576694264,
-			'url' => 'http://example.com/api/v1/order/notification',
-			'is_encrypted' => true
-		]);
+        $response = $webhooks->triggerWebhook([
+            'type' => 'order_notification',
+            'order_id' => 576694264,
+            'url' => 'http://example.com/api/v1/order/notification',
+            'is_encrypted' => true
+        ]);
 
-		$deserialized = json_decode($contents, true);
-		$response     = json_decode($response, true);
+        $deserialized = json_decode($contents, true);
+        $response     = json_decode($response, true);
 
-		$this->assertEquals($deserialized, $response);
-	}
+        $this->assertEquals($deserialized, $response);
+    }
 
-	public function testCanTriggerWebhookManuallyWithFailedResponse()
-	{
-		$webhooks    = new Webhooks($this->getAuthorization());
-		$mockHandler = new MockHandler();
-		$httpClient  = new Client(['handler' => $mockHandler]);
-		$contents    = file_get_contents(
-			__DIR__ . '/../data-fixtures/webhooks/trigger-webhook-fail.json'
-		);
+    public function testCanTriggerWebhookManuallyWithFailedResponse()
+    {
+        $webhooks    = new Webhooks($this->getAuthorization());
+        $mockHandler = new MockHandler();
+        $httpClient  = new Client(['handler' => $mockHandler]);
+        $contents    = file_get_contents(
+            __DIR__ . '/../data-fixtures/webhooks/trigger-webhook-fail.json'
+        );
 
-		$mockHandler->append(new Response(
-			200,
-			['Content-Type' => 'application/json'],
-			$contents
-		));
+        $mockHandler->append(new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $contents
+        ));
 
-		$webhooks->setHttpClient($httpClient);
+        $webhooks->setHttpClient($httpClient);
 
-		$response = $webhooks->triggerWebhook([
-			'type' => 'order_notification',
-			'order_id' => 576694264,
-			'url' => 'http://example.com/api/v1/order/notification',
-			'is_encrypted' => true
-		]);
+        $response = $webhooks->triggerWebhook([
+            'type' => 'order_notification',
+            'order_id' => 576694264,
+            'url' => 'http://example.com/api/v1/order/notification',
+            'is_encrypted' => true
+        ]);
 
-		$deserialized = json_decode($contents, true);
-		$response     = json_decode($response, true);
+        $deserialized = json_decode($contents, true);
+        $response     = json_decode($response, true);
 
-		$this->assertEquals($deserialized, $response);
-	}
+        $this->assertEquals($deserialized, $response);
+    }
 }
